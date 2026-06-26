6c8b882391b5cb51a6a7c8220fbd56b0e56c4d56
creativor <creativor@gmail.com>
2026-06-26 14:48:01 +0800

    禁用 HMR 热更新，防止网页自动刷新导致 WebSocket 断开

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0', // 允许局域网访问
+    hmr: false, // 禁用 HMR 热更新，防止网页自动刷新导致 WebSocket 断开
     proxy: {
       '/dylive': {
         target: 'https://live.douyin.com',
